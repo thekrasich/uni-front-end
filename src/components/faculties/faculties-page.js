074e1c0b0f8a899194cbb76cbbcb1b57c5072839
Stanislav Krasichinsky <thekrasich@gmail.com>
2022-11-26 17:43:57 +0200

    Added Faculties page

--- src/components/faculties/faculties-page.js
+++ src/components/faculties/faculties-page.js
@@ -1,31 +1,52 @@
 import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import Footer from "../ui/footer/footer";
 import Header from "../ui/header/header";
 
 const FacultiesPage = () => {
   const [faculties, setFaculties] = useState([]);
 
-  const getFaculties = () => {
-    axios.get("http://localhost:3000/api/faculties").then((response) => {
-      const data = response.data;
-      console.log(data);
-    }).catch(error => {
-        console.log(error)
-    });
+  const GetDepartments = (dep) =>{
+   return(
+    <div>
+      {dep.map(({ id, name}) => (
+        <p className="weed-text" key={id}>
+          {" "}
+          {name} 
+          <hr></hr>
+        </p>
+      ))}
+    </div>
+   )
   };
-  getFaculties();
+
+  useEffect(() => {
+    const getFaculties = () => {
+      axios
+        .get("http://localhost:3000/api/faculties")
+        .then((response) => {
+          const data = response.data;
+          setFaculties(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getFaculties();
+  }, []);
   return (
     <div>
       <Header />
-      <Accordion>
-        <AccordionSummary id="panel1" aria-controls="panel1content">
-          Accordion 1
-        </AccordionSummary>
-        <AccordionDetails>Text</AccordionDetails>
-      </Accordion>
-      <Footer />
+      {faculties.map(({ id, name, departments }) => (
+        <Accordion>
+          <AccordionSummary id={'1'} aria-controls="panel1content">
+            {name}
+          </AccordionSummary>
+          <AccordionDetails>
+            {GetDepartments(departments)}
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </div>
   );
 };
